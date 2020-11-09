668d240b6f237a39d23e1b23e18c9d4f79041eb8
shashank08090 <shashank08090@gmail.com>
2020-11-09 17:55:42 +0530

    Promise in javascript

---
+++ Promises.js
@@ -0,0 +1,18 @@
+
+var promise = new Promise(function(resolve, reject) { 
+  const x = 10; 
+  const y = 10;
+  if(x === y) { 
+    resolve(); 
+  } else { 
+    reject(); 
+  } 
+}); 
+  
+promise. 
+    then(function () { 
+        console.log('Success, You are a GEEK'); 
+    }). 
+    catch(function () { 
+        console.log('Some error has occured'); 
+    });
